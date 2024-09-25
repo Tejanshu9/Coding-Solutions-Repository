e28d77b452de197c630c5eca2b707b5fa1ab2333
Tejanshu9 <tejanshusekhar1046@gmail.com>
2024-09-25 20:08:32 +0530

    modified:   LeetCode/126/findLadders.cpp

--- LeetCode/126/findLadders.cpp
+++ LeetCode/126/findLadders.cpp
@@ -14,41 +14,40 @@ public:
         for(auto x : adj[beginWord])
             DFS(x, endWord, adj);
         
-        path.pop_back(); //pop current word to backtrack
+        path.pop_back(); 
     }
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,unordered_set<string>>adj; //adjacency list
-        unordered_set<string> dict(wordList.begin(), wordList.end()); //insert all the strings in set
+        unordered_map<string,unordered_set<string>>adj; 
+        unordered_set<string> dict(wordList.begin(), wordList.end()); 
          if(!dict.count(endWord) || dict.empty()) return ans;
-		//step-1 => Find min depth using BFS
-        queue<string>q;  //for BFS Traversal
+		
+        queue<string>q;  
         q.push(beginWord);
         unordered_map<string,int>visited;
-        visited[beginWord] = 0; //start node will always at level 0
-        
+        visited[beginWord] = 0; 
         while(!q.empty()){
             string curr = q.front();
             q.pop();
             string temp = curr;
             
-            for(int i = 0; i < curr.size(); i++){ //check all characters
-                for(char c = 'a'; c <= 'z'; c++){ //try all possible 26 letters
-                    if(temp[i] == c) continue;  //skip if letter is same as original word
+            for(int i = 0; i < curr.size(); i++){ 
+                for(char c = 'a'; c <= 'z'; c++){ 
+                    if(temp[i] == c) continue;  
                     temp[i] = c;
                     
-                    if(dict.count(temp) > 0){ // check if new word is present in wordList
-                        if(visited.count(temp) == 0){ //check if new word was already visited
+                    if(dict.count(temp) > 0){ 
+                        if(visited.count(temp) == 0){ 
                             visited[temp] = visited[curr] + 1;
                             q.push(temp);
                             adj[temp].insert(curr);
                         }
-                        else if(visited[temp] == visited[curr] + 1) //if already visited and new word is child
+                        else if(visited[temp] == visited[curr] + 1) 
                             adj[temp].insert(curr);    
                     }
                 }
             }
         }
-       // step-2 => find all min depth possible paths using DFS
+       
        if (!adj.count(endWord)) return ans;
         DFS(endWord, beginWord, adj);
         return ans;
